0e465cca019ab0abddf5341a94d52140e699cce5
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2023-06-19 22:58:29 -0700

    Refactored 2090 to loop once

--- 2090 K radius.ts
+++ 2090 K radius.ts
@@ -1,25 +1,16 @@
 function getAverages(nums: number[], k: number): number[] {
-  const answerArr: number[] = [];
+  const answerArr: number[] = Array(nums.length).fill(-1);
   const dia = 2 * k + 1;
   let sum = 0;
   for (let i = 0; i < nums.length; i++) {
-    //let's first add the number, then push only if it's past diameter and then append the remaining ones?
-    if (i < k || i >= nums.length - k) {
-      answerArr.push(-1);
-    } else {
-      // if this is the first average actually average it out
-      if (i === 0 || answerArr[i - 1] === -1) {
-        //make an average
-        for (let j = i - k; j <= i + k; j++) {
-          sum += nums[j];
-        }
-        answerArr.push(Math.floor(sum / dia));
-      } else {
-        //otherwise, previous average minus prev plus current
-        sum += nums[i + k] - nums[i - k - 1];
-        answerArr.push(Math.floor(sum / dia));
-      }
+    // start summing up the values
+    sum += nums[i];
+    if (i >= dia) sum -= nums[i - dia];
+
+    // add the one i+k away
+    if (i >= dia - 1) {
+      answerArr[i - k] = Math.floor(sum / dia);
     }
   }
-  return answerArr;
+  return k ? answerArr : nums;
 }
